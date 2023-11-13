1e9ed2eae94cf6bb1da3e0fe28dd441ebb55fba0
ElRomario <hellishassailant007@gmail.com>
2023-11-13 16:24:03 +0300

    HWS DONE

---
+++ HW12Collections/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+/*C# 12 (23 октября 2023)
+Словари
+Срок: к следующему занятию, четвергу 25 октября
+
+1. Напишите класс автоматического переводчика фраз с русского на английский и обратно. Переводчик содержит в себе два словаря: англо-русский и русско-английский.
+
+Программа заполняет словарь парами: кошка->cat в одну сторону и cat->кошка в другую. Заполните словари 10-20 часто встречающимися парами.
+
+2. Затем переводчит криступает к работе. Пользователь выбирает режим: англо-русский или русско-английский, затем вводит фразу, которую надо перевести. Переводчик выполняет перевод дословно. Он не владеет такими тонкостями, как падежи.
+
+I have a cat -> Я иметь некоторый кошка
+
+Пользователь вводит фразу, получает перевод; вводит ещё одну фразу, получает перевод; и так далее.
+
+3*. Необязательная задача на повышенную оценку. Если пользователь вводит незнакомое слово, программа спрашивает его перевод и добавляет в свои словари. Потом она справляется с переводом фразы.
+*/
+namespace HW12Collections
+{
+    class Program
+    {
+        private static void TranslateToRus(Translator translator)
+        {
+            Console.WriteLine($"Введите фразу для перевода: ");
+            string input = Console.ReadLine();
+            translator.TranslateToRussian(input);
+            Console.WriteLine();
+            Console.WriteLine(translator.TranslateToRussian(input));
+        }
+
+        private static void TranslateToEng(Translator translator)
+        {
+            Console.WriteLine($"Введите фразу для перевода: ");
+            string input = Console.ReadLine();
+            translator.TranslateToEnglish(input);
+            Console.WriteLine();
+            Console.WriteLine(translator.TranslateToEnglish(input));
+        }
+
+
+        private static void mode(Translator translator)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Выберите нужный вам режим перевода: ");
+                Console.WriteLine($"1 - русский -> английский ");
+                Console.WriteLine($"2 - английский -> русский ");
+                string text = Console.ReadLine();
+                int choice = int.Parse(text);
+
+                switch (choice)
+                {
+                    case 1:
+                        TranslateToEng(translator);
+                        break;
+                    case 2:
+                        TranslateToRus(translator);
+                        break;
+                    default:
+                        Console.WriteLine($"Введена неверная цифра!!");
+                        break;
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Translator translator = new Translator();
+            mode(translator);
+
+        }
+    }
+}
---
+++ HW12Collections/Translator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12Collections
+{
+    internal class Translator
+    {
+        private Dictionary<string, string> toRus = new Dictionary<string, string>()
+        {
+            {"club","клуб" },
+            {"age","возраст" },
+            {"baby","малыш" },
+            {"birth","рождение" },
+            {"boy","мальчик" },
+            {"child","ребенок" },
+            {"childhood","детство" },
+            {"buddy","приятель" },
+            {"girl","девочка" },
+            {"human","человек" },
+            {"life","жизнь" },
+            {"woman","молодость" },
+            {"welcome","добро пожаловать" },
+            {"to","к" },
+            {"the","тот" },
+        };
+
+        private Dictionary<string, string> toEng = new Dictionary<string, string>()
+        {
+            {"клуб","club" },
+            {"возраст","age" },
+            {"малыш","baby" },
+            {"рождение","birth" },
+            {"мальчик","boy" },
+            {"ребенок","child" },
+            {"детство","childhood" },
+            {"приятель","buddy" },
+            {"девочка","girl" },
+            {"человек","human" },
+            {"жизнь","life" },
+            {"молодость","woman" },
+            {"добро пожаловать","welcome" },
+            {"к","to" },
+            {"тот","the" },
+        };
+
+
+        public string TranslateToRussian(string input)
+        {
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (toRus.ContainsKey(words[i]))
+                {
+                    words[i] = toRus[words[i]];
+                }
+            }
+            return string.Join(" ", words);
+        }
+
+        public string TranslateToEnglish(string input)
+        {
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (toEng.ContainsKey(words[i]))
+                {
+                    words[i] = toEng[words[i]];
+                }
+            }
+            return string.Join(" ", words);
+        }
+
+
+    }
+}
